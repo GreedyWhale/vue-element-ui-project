8f31658532e38686bc92d326bcd9310debdf5682
caisr <caisr@innapp.cn>
2018-07-09 18:50:13 +0800

    增加默认错误返回

--- src/utils/request/filterStatusCode.js
+++ src/utils/request/filterStatusCode.js
@@ -1,15 +1,19 @@
 import { showDialog } from '../dialog'
 
 const filterStatusCode = error => {
-  switch (error.response.status) {
-    case 404:
-      return showDialog('请求错误,未找到该资源')
-    case 500:
-      return showDialog('服务器端出错')
-    case 503:
-      return showDialog('服务不可用')
-    default:
-      return showDialog('恭喜你，获得解锁神秘错误成就')
+  if (error && error.response) {
+    switch (error.response.status) {
+      case 404:
+        return showDialog('请求错误,未找到该资源')
+      case 500:
+        return showDialog('服务器端出错')
+      case 503:
+        return showDialog('服务不可用')
+      default:
+        return showDialog('恭喜你，获得解锁神秘错误成就')
+    }
+  } else {
+    return showDialog('请求失败')
   }
 }
 
